686fa5f2d1a958bfa09b2c710d457c9cc13d6f54
Dixie J <devildjoseph666@gmail.com>
2022-02-24 21:42:38 +0800

    Skinslist response prettified

--- pages/api/skinslist/index.js
+++ pages/api/skinslist/index.js
@@ -2,6 +2,6 @@ const fs = require('fs');
 
 export default async function readSkins(req, res) {
     const file = await fetch("https://raw.githubusercontent.com/weedeej/vtools-next/main/pages/api/skinslist/Purchaseables.json", {method: 'GET'});
-    if (Object.keys(req.query).length === 0) return res.status(200).json(await file.json());
-    return res.status(200).send(await file.json());
+    if (Object.keys(req.query).length === 0) return res.status(200).json(JSON.stringify(await file.json(), null, 4));
+    return res.status(200).send(file.body);
 }
