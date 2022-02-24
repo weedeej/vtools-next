852a40f9002c8fde14206e860fcd0eb1e7b4b310
Dixie J <devildjoseph666@gmail.com>
2022-02-24 21:28:47 +0800

    Removed default headers as I'm using .json()

--- next.config.js
+++ next.config.js
@@ -1,12 +1,3 @@
 module.exports = {
   reactStrictMode: true,
-  async headers()
-  {
-    return [
-      {
-        source: ":/api*",
-        headers: { "content-type": "application/json" }
-      }
-    ]
-  }
 }
