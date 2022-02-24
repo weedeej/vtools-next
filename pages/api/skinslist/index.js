9b0c7f20b374702a2e6a700f3ce53ed676e0d53b
Dixie J <devildjoseph666@gmail.com>
2022-02-24 21:35:49 +0800

    Skinlist fetch fix

--- pages/api/skinslist/index.js
+++ pages/api/skinslist/index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 export default async function readSkins(req, res) {
-    const file = fs.readFileSync('./pages/api/skinslist/Purchaseables.json');
-    if (Object.keys(req.query).length === 0) return res.status(200).json(file);
-    return res.status(200).send(file);
+    const file = await fetch("https://raw.githubusercontent.com/weedeej/vtools-next/main/pages/api/skinslist/Purchaseables.json", {method: 'GET'});
+    if (Object.keys(req.query).length === 0) return res.status(200).json(await file.json());
+    return res.status(200).send(await file.json());
 }
